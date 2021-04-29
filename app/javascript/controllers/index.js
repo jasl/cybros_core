46d27ca7a60c92c38b3c85a1479fab9e103eaf0f
Eric-Guo <eric.guocz@gmail.com>
2021-04-29 10:59:02 +0800

    Go stimulus 2.0.0

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,4 @@
-// Load all the controllers within this directory and all subdirectories.
+// Load all the controllers within this directory and all subdirectories. 
 // Controller files must be named *_controller.js.
 
 import { Application } from "stimulus"
@@ -7,4 +7,3 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
-
